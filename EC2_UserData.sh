094f33827cacf023f662065d4ce04faf78908bb9
YaeshwanthUrumaiya <yaeshwanthurumaiya@outlook.com>
2024-10-13 21:49:03 +0530

    Final Ver

--- EC2_UserData.sh
+++ EC2_UserData.sh
@@ -25,5 +25,4 @@ pip install --no-cache-dir torch==2.4.1 --index-url https://download.pytorch.org
 pip install --no-cache-dir --no-deps mpmath urllib3 typing_extensions threadpoolctl sympy safetensors regex pyyaml Pillow packaging numpy networkx MarkupSafe joblib idna fsspec filelock colorama charset-normalizer certifi tqdm scipy requests jinja2 scikit-learn huggingface_hub tokenizers transformers sentence_transformers --break-system-packages
 pip install --no-cache-dir faiss-cpu langchain langchain-aws langchain-community langchain-core rank-bm25 --break-system-packages
 
-# Exit 
-exit
+echo "Setup completed successfully!"
